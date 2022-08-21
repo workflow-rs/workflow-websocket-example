a82c60211cc2f3e654c1ad4ac95df5996992bbe8
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-08-22 00:53:37 +0300

    cleanup example code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,52 +1,44 @@
-use std::{time::Duration, ops::DerefMut};
+use std::time::Duration;
 
 use workflow_log::*;
 use workflow_websocket::client::{
-    WebSocket, Settings, Result, Message, DispatchMessage,
-    DispatchStrategy
+    WebSocket, Result, Message
 };
 
 #[tokio::main]
 async fn main() -> Result<()> {
 
-    // let settings = Settings {
-    //     strategy : DispatchStrategy::Ack
-    // };
-    let ws = WebSocket::new("ws://localhost:9090", Settings::default())?;
-    let ws = WebSocket::new("ws://localhost:9090", Settings::default())?;
-
-    ws.connect().await?;
-
-    // let tx = ws.dispatcher_tx.clone();
+    let ws = WebSocket::new("ws://localhost:9090")?;
+    ws.connect(true).await?;
 
     let ws_ = ws.clone();
     workflow_core::task::spawn(async move {
         let mut seq = 0;
         loop {
-            log_trace!("send loop starting {}" ,seq);
-            let msg = format!("hello world {}", seq).into();
-            ws_.send(Message::Text(msg)).await.expect("error sending message ");
-            // tokio::time::sleep(Duration::from_millis(0)).await;
-            // workflow_core::task::yield_now().await;
-            log_trace!("send loop iterating {}", seq);
+            log_trace!("▷ sending message {}" ,seq);
+            let msg = format!("message {}", seq).into();
+            // let result = ws_.post(Message::Text(msg)).await;;
+            let result = ws_.send(Message::Text(msg)).await;;
+            match result {
+                Ok(_) => {  },
+                Err(err) => {
+                    log_error!("Error sending message: {}", err);
+                }
+            }
+
+            workflow_core::task::sleep(Duration::from_millis(1000)).await;
+
             seq += 1;
         }
-        log_trace!("send loop exit");
     });
 
 
     let ws_ = ws.clone();
-    // tokio::task::spawn_blocking(|| {
-    //     async move {
-
-            // let mut receiver_rx = ws_.receiver_rx.lock();
     loop {
-        log_trace!("loop running");
-        // let message = receiver_rx.deref_mut().recv().await.unwrap();
-        let message = ws_.receiver_rx.recv().await.unwrap();
-        log_info!("receiving message: {:?}", message);
+        let message = ws_.recv().await.unwrap();
+        log_info!("◁ receiving message: {:?}", message);
     }
-            log_trace!("loop exit");
-    Ok(())
+
+    // Ok(())
 }
 
